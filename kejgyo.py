6b0dc4b338c7ffebb616362a67c1b09f604d9f9f
GinalZsolt <ginalzsolt@turr.hu>
2021-12-14 12:08:51 +0100

    kész a szűrés

--- kejgyo.py
+++ kejgyo.py
@@ -1,22 +1,7 @@
-import csv
+from os import system
+system('cls')
 
-def elso():
-    with open('otos.csv', 'r') as file:
-        reader =csv.reader(file)
-        array=[]
-        first=True
-        for row in reader:
-            if first:
-                first=not first
-                continue
-            array.append(row)
-        numbre=str(array[0]).split(';')
-        print(numbre[11:16])
-        numbre=str(array[1]).split(';')
-        print(numbre[11:16])
-        
-
-file =open(r"D:/Git GZs/Új mappa/LOTTO/otos.csv")
+file =open(r"D:/Git GZs/LOTTO/otos.csv")
 elso=True
 data_array=[]
 for row in file.readlines():
@@ -31,6 +16,3 @@ for row in file.readlines():
     data.append(int(row.split(';')[15]))
 
     data_array.append(data)
-
-
-
